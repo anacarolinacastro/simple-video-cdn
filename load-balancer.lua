9cf0f7f716a1311747945ebff9495f37941bc03b
Ana Carolina Castro <ana.castro@corp.globo.com>
2019-06-01 12:03:13 -0300

    Test with lua + nginx

---
+++ load-balancer.lua
@@ -0,0 +1,20 @@
+local function round_robin ()
+end
+
+local function lead_conn ()
+end
+
+local function choose_host_hash ()
+end
+
+local function choose_host_consistent_hash ()
+end
+
+local function choose_host_consistent_hash_bound_load ()
+end
+
+local function process()
+  ngx.say('the spice must flow')
+end
+
+return process
