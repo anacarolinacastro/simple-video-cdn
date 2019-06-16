2068a6f75970a21d974666f7fd25998c0f1fef3d
Ana Carolina Castro <ana.castro@corp.globo.com>
2019-06-16 18:49:16 -0300

    Implement round robin

--- lb/load_balancer.lua
+++ lb/load_balancer.lua
@@ -1,4 +1,5 @@
 local load_balancer = {}
+local index_reference = 1
 
 local function get_algoritm()
     return os.getenv("LB_ALGORITM")
@@ -16,14 +17,23 @@ function get_health_servers()
     -- ngx.log(ngx.ERR, "teste")
     -- local redis = require 'redis'
     -- local client = redis.connect('172.22.0.100', 6379)
-    -- local response = client:ping()  
-    
+    -- local response = client:ping()
+
     return {"8090", "8091"}
+end
+
+load_balancer.random = function()
+    ports = get_health_servers()
+    port = ports[math.random(1,#ports)]
+
+    return "http://0.0.0.0:" .. port
 end
 
 load_balancer.round_robin = function()
     ports = get_health_servers()
-    port = ports[math.random(1,#ports)]
+    port_index = math.fmod(index_reference,#ports) + 1
+    port = ports[port_index]
+    index_reference = index_reference + 1
 
     return "http://0.0.0.0:" .. port
 end
