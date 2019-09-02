4d916b839a64e691e429070a84ab30c9b66e8bd1
Ana Carolina Castro <ana.castro@corp.globo.com>
2019-09-01 23:30:59 -0300

    Add high audience signal

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 for i in $(seq 1 $2)
 do
-    docker run --net="host" --rm anafrombr/go-wrk -redir -T 5000 -c 10 -d 30  http://0.0.0.0:80/live/hls/signal-$i/index.m3u8 > results/benchmark/signal-$1-$i.log &
+    count=100
+    if [ $i -eq "10" ]
+    then
+        count=500
+    fi
+
+    docker run --net="host" --rm anafrombr/go-wrk -redir -T 5000 -c $count -d 5  http://0.0.0.0:80/live/hls/signal-$i/index.m3u8 > results/benchmark/signal-$1-$i.log &
 done
